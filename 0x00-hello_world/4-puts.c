3f59747d1a8479aa32872ac5ff9fe60e3fa40e68
tochileo <tochukwuadjanohoun@gmail.com>
2022-09-08 15:04:22 -0700

    hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 * main - print the string in the put function
 *
@@ -9,6 +8,6 @@
 */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        Return (0);
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
 }
