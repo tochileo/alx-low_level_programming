d0b7807237a965e46fda0d95938050d254627884
tochileo <tochukwuadjanohoun@gmail.com>
2022-09-14 11:35:41 -0700

    time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,43 +1,48 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_times_table - Prints the n times table
+ * print_times_table - Prints a multiplication table up to param
+ * @n: The number to be treated
  *
- * @n: number times table (0 < n <= 15)
- *
- * Return: no return
+ * Return: Number matrix
  */
 void print_times_table(int n)
 {
-	int a, b, op;
-
-	if (n >= 0 && n <= 15)
+	int x, y, z;
+ 
+	if (n >= 0 && n <= 14)
 	{
-		for (a = 0; a <= n; a++)
+		for (x = 0; x <= n; x++)
 		{
-			putchar(48);
-			for (b = 1; b <= n; b++)
+			for (y = 0; y <= n; y++)
 			{
-				op = a * b;
-				putchar(44);
-				_putchar(32);
-				if (op <= 9)
+				z = x * y;
+				if (z > 99)
 				{
+					_putchar(',');
 					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
-				else if (op <= 99)
+				else if (z > 9)
 				{
+					_putchar(',');
 					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
 				else
 				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
 				}
 			}
 			_putchar('\n');
